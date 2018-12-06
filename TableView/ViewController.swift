f7b2a89d2d25fd833f2c9f6fd39b4fe73c72252b
Amol Dhage <amoltdhage@Amols-MacBook-Pro.local>
2018-12-06 16:25:51 +0530

    Updated

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITAB{
+class ViewController: UIViewController, UITableViewDelegate , UITableViewDataSource {
 
+    var array1 = ["Akshay","Bhushan","Abhijeet","Amol"]
+    
+    var array2 = ["Gulhane","Dharmale","Ghate","Dhage"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array2.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //UITableViewStyle *cell
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = array1[indexPath.row]
+        cell?.detailTextLabel?.text = array2[indexPath.row]
+        
+     return cell!
+        
+        
+        
+    }
 
 }
 
