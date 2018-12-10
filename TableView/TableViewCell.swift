4de85dd522febcb8f912f581ddd47a1fcc90e3b2
Amol Dhage <amoltdhage@gmail.com>
2018-12-10 18:54:20 +0530

    README.md Updated

---
+++ TableView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  TableView
+//
+//  Created by Amol Dhage on 10/12/18.
+//  Copyright © 2018 AmolTDhage. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
